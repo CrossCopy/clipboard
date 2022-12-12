1ec7dd3e6907936857cd487d4936138b6e27fcc6
Huakun Shen <huakun.shen@huakunshen.com>
2022-12-12 07:44:42 -0500

    Redesigned the entire package (#2)
    * Implemented IPC through TCP socket
    
    * Fix partial screenshot issue. It's due to TCP packet size limit. Have to concatenate packets together.
    
    * Implement read write operations for text and image using child process
    
    * Move some IPC demo code to example folder under go-clipboard
    
    * Redesigned and implemented npm package API.
    
    * Update CI, only on Windows
    
    * Fix CI
    
    * Update package version.
    
    * Make package verison beta

---
+++ __tests__/index.spec.ts
@@ -0,0 +1,37 @@
+import cb from "../index";
+import { base64img } from "./data";
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+describe("Test Text Clipboard", () => {
+  it("Synchronous Clipboard Text Methods", async () => {
+    cb.writeTextSync("abc");
+    await sleep(1);
+    const text = cb.readTextSync();
+    expect(text).toBe("abc");
+  });
+
+  it("Async Clipboard Text", async () => {
+    await cb.writeText("def");
+    await sleep(1);
+    expect(await cb.readText()).toBe("def");
+  });
+
+  it("Synchronous Cliipboard Image Methods", async () => {
+    cb.writeImageSync(base64img);
+    await sleep(100);
+    expect(cb.readImageBase64Sync()).toBe(base64img);
+    cb.writeImageSync(base64img);
+    await sleep(100);
+    expect((cb.readImageSync()).toString("base64")).toBe(base64img);
+  });
+
+  it("Asynchronous Cliipboard Image Methods", async () => {
+    await cb.writeImage(base64img);
+    await sleep(100);
+    expect(await cb.readImageBase64()).toBe(base64img);
+    await cb.writeImage(base64img);
+    await sleep(100);
+    expect((await cb.readImage()).toString("base64")).toBe(base64img);
+  });
+});
--- demo.ts
+++ demo.ts
@@ -1,19 +1,36 @@
-import clipboardEventListener from "./index";
+import clipboard from "./index";
+import fs from "node:fs";
+import { base64img } from "./__tests__/data";
 
-clipboardEventListener.on("text", () => {
-  console.log("Clipboard Text Updated");
-});
+(async function () {
+  console.log(clipboard.readTextSync());
+  console.log(await clipboard.readText());
+  const imgBuf = clipboard.readImageSync();
+  // console.log(imgBuf.toString("base64"));
+  // console.log(clipboard.readImageBase64Sync());
+  // await clipboard.writeImage(base64img); // add fake image to clipboard
+  clipboard.writeImageSync(base64img); // add fake image to clipboard
+  console.log(""); // give some time
+  console.assert(clipboard.readImageBase64Sync() === base64img);
 
-clipboardEventListener.on("image", () => {
-  console.log("Clipboard Image Updated");
-});
+  // * test readimage
+  clipboard.writeImageSync(base64img);
+  console.log();
+  console.assert(
+    (await clipboard.readImage()).toString("base64") === base64img
+  );
 
-clipboardEventListener.on("open", (data) => {
-  console.log(data);
-});
-
-clipboardEventListener.startListening();
-
-setTimeout(() => {
-  clipboardEventListener.stopListening();
-}, 10000);
+  await clipboard.writeImage(base64img);
+  console.log();
+  console.assert((await clipboard.readImageBase64()) === base64img);
+  clipboard.on("text", (text) => {
+    console.log(text);
+  });
+  clipboard.on("image", (data) => {
+    fs.writeFileSync("test.png", data);
+  });
+  clipboard.listen();
+  setTimeout(() => {
+    clipboard.close();
+  }, 10000);
+})();
