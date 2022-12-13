14c0519f4dc011f37c34de10eb22b62def4adf94
Huakun Shen <huakun.shen@huakunshen.com>
2022-12-12 21:03:28 -0500

    Refactored Code

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export enum ClipboardOperation {
+  READ_TEXT = "READ_TEXT",
+  WRITE_TEXT = "WRITE_TEXT",
+  READ_IMAGE = "READ_IMAGE",
+  WRITE_IMAGE = "WRITE_IMAGE",
+}
+
+export enum WatchEvent {
+  TEXT_CHANGED = "TEXT_CHANGED",
+  IMAGE_CHANGED = "IMAGE_CHANGED",
+}
